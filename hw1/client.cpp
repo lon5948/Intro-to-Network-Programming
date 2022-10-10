50b1f6fad594d81e57146c857664bf1e544e94c7
lon5948 <a0988282303@gmail.com>
2022-10-10 18:30:43 +0800

    check answer not finish

--- hw1/client.cpp
+++ hw1/client.cpp
@@ -11,7 +11,6 @@
 using namespace std;
 
 vector<string> split(string str) {
-
     vector<string> result;
     stringstream ss(str);
     string token;
@@ -22,12 +21,23 @@ vector<string> split(string str) {
     return result;
 }
 
+void Welcome(int TCP_socket) {
+    char receiveMessage[512] = {};
+    int errR = recv(TCP_socket, receiveMessage, sizeof(receiveMessage), 0);
+    if (errR == -1) {
+        cout << "[Error] Fail to receive message from the server." << endl;
+    }
+    else {
+        cout << receiveMessage << endl;
+    }
+}
+
 void Exit(int TCP_socket, int UDP_socket) {
-    char sendMessage[] = { "exit" };
+    char sendMessage[512] = { "exit" };
 
     int errS = send(TCP_socket,sendMessage,sizeof(sendMessage),0);
     if (errS == -1) {
-        cout << "Error: Fail to send message to the server." << endl;
+        cout << "[Error] Fail to send message to the server." << endl;
     }
     //logout first
     close(TCP_socket);
@@ -36,27 +46,21 @@ void Exit(int TCP_socket, int UDP_socket) {
 
 void Register(int UDP_socket, string commandInput, struct sockaddr_in &serverAddr) {
     int len = commandInput.length();
-    char sendMessage[len] = {};
+    char sendMessage[512] = {};
     commandInput.copy(sendMessage, len);
-    char receiveMessage[128] = {};
+    char receiveMessage[512] = {};
 
     int errS = sendto(UDP_socket, sendMessage, sizeof(sendMessage), 0, (const struct sockaddr*) &serverAddr, sizeof(serverAddr));
     if (errS == -1) {
-        cout << "Error: Fail to send message to the server." << endl;
+        cout << "[Error] Fail to send message to the server." << endl;
     }
 
     int errR = recvfrom(UDP_socket, receiveMessage, sizeof(receiveMessage), 0, (const struct sockaddr*) &serverAddr, sizeof(serverAddr));
     if (errR == -1) {
-        cout << "Error: Fail to receive message from the server." << endl;
+        cout << "[Error] Fail to receive message from the server." << endl;
     }
-    else if (receiveMessage == "Success") {
-        cout << "Register successfully." << endl;
-    }
-    else if (receiveMessage == "Fail1") {
-        cout << "Username is already used." << endl;
-    }
-    else if (receiveMessage == "Fail2") {
-        cout << "Email is already used." << endl;
+    else {
+        cout << receiveMessage << endl;
     }
 }
 
@@ -68,24 +72,15 @@ void Login(int TCP_socket, string commandInput) {
 
     int errS = send(TCP_socket, sendMessage, sizeof(sendMessage), 0);
     if (errS == -1) {
-        cout << "Error: Fail to send message to the server." << endl;
+        cout << "[Error] Fail to send message to the server." << endl;
     }
 
     int errR = recv(TCP_socket, receiveMessage, sizeof(receiveMessage), 0);
     if (errR == -1) {
-        cout << "Error: Fail to receive message from the server." << endl;
+        cout << "[Error] Fail to receive message from the server." << endl;
     }
-    else if (receiveMessage == "Success") {
-        cout << "Welcome," << username/receiveMessage << "." << endl;
-    }
-    else if (receiveMessage == "Fail1") {
-        cout << "Please logout first." << endl;
-    }
-    else if (receiveMessage == "Fail2") {
-        cout << "Username not found." << endl;
-    }
-    else if (receiveMessage == "Fail3") {
-        cout << "Password not correct." << endl;
+    else {
+        cout << receiveMessage << endl;
     }
 }
 
@@ -95,112 +90,75 @@ void Logout(int TCP_socket) {
 
     int errS = send(TCP_socket, sendMessage, sizeof(sendMessage), 0);
     if (errS == -1) {
-        cout << "Error: Fail to send message to the server." << endl;
+        cout << "[Error] Fail to send message to the server." << endl;
     }
 
     int errR = recv(TCP_socket, receiveMessage, sizeof(receiveMessage), 0);
     if (errR == -1) {
-        cout << "Error: Fail to receive message from the server." << endl;
+        cout << "[Error] Fail to receive message from the server." << endl;
     }
-    else if (receiveMessage == "Success") {
-        cout << "Bye," << username/receiveMessage << "." << endl;
-    }
-    else if (receiveMessage == "Fail") {
-        cout << "Please login first." << endl;
+    else {
+        cout << receiveMessage << "." << endl;
     }
 }
 
 void Rule(int UDP_socket, struct sockaddr_in &serverAddr) {
-    char sendMessage[] = { "rule" };
-    char receiveMessage[1024] = {};
+    char sendMessage[] = { "game-rule" };
+    char receiveMessage[512] = {};
 
     int errS = sendto(UDP_socket, sendMessage, sizeof(sendMessage), 0, (const struct sockaddr*) &serverAddr, sizeof(serverAddr));
     if (errS == -1) {
-        cout << "Error: Fail to send message to the server." << endl;
+        cout << "[Error] Fail to send message to the server." << endl;
     }
 
     int errR = recvfrom(UDP_socket, receiveMessage, sizeof(receiveMessage), 0, (const struct sockaddr*) &serverAddr, sizeof(serverAddr));
     if (errR == -1) {
-        cout << "Error: Fail to receive message from the server." << endl;
+        cout << "[Error] Fail to receive message from the server." << endl;
     }
-    else if (receiveMessage == "game-rule") {
-        cout << "1. Each question is a 4-digit secret number." << endl;
-        cout << "2. After each guess, you will get a hint with the following information:" << endl;
-        cout << "2.1 The number of \"A\", which are digits in the guess that are in the correct position." << endl;
-        cout << "2.2 The number of \"B\", which are digits in the guess that are in the answer but are in the wrong position." << endl;
-        cout << "The hint will be formatted as \"xAyB\"." << endl;
-        cout << "3. 5 chances for each question." << endl;
-    }
-}
-
-void Start(int TCP_socket, string commandInput, struct sockaddr_in &serverAddr) {
-    int len = commandInput.length();
-    char sendMessage[len] = {};
-    commandInput.copy(sendMessage, len);
-    char receiveMessage[128] = {};
-
-    int errS = send(TCP_socket, sendMessage, sizeof(sendMessage), 0);
-    if (errS == -1) {
-        cout << "Error: Fail to send message to the server." << endl;
-    }
-
-    int errR = recv(UDP_socket, receiveMessage, sizeof(receiveMessage), 0);
-    if (errR == -1) {
-        cout << "Error: Fail to receive message from the server." << endl;
-    }
-    else if (receiveMessage == "Fail1") {
-        cout << "Please login first." << endl;
-    }
-    else if (receiveMessage == "Fail2") {
-        cout << "Usage: start-game <4-digit number>" << endl;
-    }
-    else if (receiveMessage == "Start") {
-        cout << "Please typing a 4-digit number:" << endl;
+    else {
+        cout << receiveMessage << endl;
     }
 }
 
 void Start(int TCP_socket, string commandInput) {
     int len = commandInput.length();
-    char sendMessage[len] = {};
+    char sendMessage[512] = {};
     commandInput.copy(sendMessage, len);
     char receiveMessage[128] = {};
 
     int errS = send(TCP_socket, sendMessage, sizeof(sendMessage), 0);
     if (errS == -1) {
-        cout << "Error: Fail to send message to the server." << endl;
+        cout << "[Error] Fail to send message to the server." << endl;
     }
 
     int errR = recv(TCP_socket, receiveMessage, sizeof(receiveMessage), 0);
     if (errR == -1) {
-        cout << "Error: Fail to receive message from the server." << endl;
+        cout << "[Error] Fail to receive message from the server." << endl;
     }
-    else if (receiveMessage == "Fail1") {
-        cout << "Please login first." << endl;
-    }
-    else if (receiveMessage == "Fail2") {
-        cout << "Usage: start-game <4-digit number>" << endl;
-    }
-    else if (receiveMessage == "Start") {
-        cout << "Please typing a 4-digit number:" << endl;
-        Game(TCP_socket);
+    else {
+        cout << receiveMessage << endl;
+        if (receiveMessage == "Please typing a 4-digit number:") {
+            Game(TCP_socket);
+        }
     }
 }
 
 void Game(int TCP_socket) {
     string number;
     char sendMessage[4] = {};
-    char receiveMessage[128] = {};
+    char receiveMessage[512] = {};
 
     while(cin >> number) {
-        number.copy(sendMessage, 4);
+        int len = number.length();
+        number.copy(sendMessage, len);
         int errS = send(TCP_socket, sendMessage, sizeof(sendMessage), 0);
         if (errS == -1) {
-            cout << "Error: Fail to send message to the server." << endl;
+            cout << "[Error] Fail to send message to the server." << endl;
         }
 
         int errR = recv(TCP_socket, receiveMessage, sizeof(receiveMessage), 0);
         if (errR == -1) {
-            cout << "Error: Fail to receive message from the server." << endl;
+            cout << "[Error] Fail to receive message from the server." << endl;
         }
         else if (receiveMessage == "Correct") {
             cout << "You got the answer!" << endl;
@@ -241,13 +199,13 @@ int main(int argc, char* argv[]) {
     // create TCP and UDP socket  
     TCP_socket = socket(AF_INET, SOCK_STREAM, 0); 
     if (TCP_socket == -1) {
-        cout << "Error: Fail to create a TCP socket." << endl;
+        cout << "[Error] Fail to create a TCP socket." << endl;
         return 0;
     }  
 
     UDP_socket = socket(AF_INET, SOCK_DGRAM, 0); 
     if (UDP_socket == -1) {
-        cout << "Error: Fail to create a UDP socket." << endl;
+        cout << "[Error] Fail to create a UDP socket." << endl;
         return 0;
     } 
 
@@ -258,64 +216,42 @@ int main(int argc, char* argv[]) {
     serverAddr.sin_addr.s_addr = inet_addr(serverIP); 
 
     // connect to a remote host
-    int err = connect(TCP_socket, (struct sockaddr*) &serverAddr, sizeof(serverAddr));  
-    if (err == -1) {
-        cout << "Error: Fail to connect server." << endl;
+    int errC = connect(TCP_socket, (struct sockaddr*) &serverAddr, sizeof(serverAddr));  
+    if (errC == -1) {
+        cout << "[Error] Fail to connect server." << endl;
         return 0;
-    }
-    else if (err == 0) {
-        cout << "*****Welcome to Game 1A2B*****" << endl;
     }
     
     string commandInput = "";
     string
     vector<string> command;
 
+    Welcome(TCP_socket);
+
     while (getline(cin, commandInput)) {
         command = split(commandInput);
         
         if (command[0] == "exit") {
-            if (command.size() != 1){
-                cout << "Usage: exit" << endl;
-            }
-            else {
-                Exit(TCP_socket, UDP_socket);
-                return 0;
-            }
+            Exit(TCP_socket, UDP_socket);
+            return 0;
         }
         else if (command[0] == "register") {
-            if (command.size() != 4){
-                cout << "Usage: register <username> <email> <password>" << endl;
-            }
-            else {
-                Register(UDP_socket, commandInput, serverAddr);
-            }
+            Register(UDP_socket, commandInput, serverAddr);
         }
         else if (command[0] == "login") {
-            if (command.size() != 3){
-                cout << "Usage: login <username> <password>" << endl;
-            }
-            else {
-                Login(TCP_socket, commandInput);
-            }
+            Login(TCP_socket, commandInput);
         }
         else if (command[0] == "logout") {
-            if (command.size() != 1){
-                cout << "Usage: logout" << endl;
-            }
-            else {
-                Logout(TCP_socket);
-            }
+            Logout(TCP_socket);
         }
         else if (command[0] == "game-rule") {
-            if (command.size() != 1){
-                cout << "Usage: game-rule" << endl;
-            }
-            else {
-                Rule(UDP_socket, serverAddr);
+            Rule(UDP_socket, serverAddr);
         }
         else if (command[0] == "start-game") {
             Start(TCP_socket, commandInput);
+        }
+        else {
+            cout << "command not found." << endl;
         }
     }
     return 0;
--- hw1/server.cpp
+++ hw1/server.cpp
@@ -1,21 +1,296 @@
 #include <iostream>  
+#include <vector> 
+#include <map> 
+#include <time.h>
+#include <cstring> 
+#include <sstream>
 #include <unistd.h>  
 #include <sys/types.h>  
 #include <sys/socket.h>  
 #include <netinet/in.h>  
 #include <arpa/inet.h>  
+#include <pthread.h> 
 
 using namespace std;
 
 #define MAX_CLIENT 10
 
+map<string,string> nameMap; // < username , password >
+map<string,string> emailMap; // < email , username >
+
+vector<string> split(string str) {
+    vector<string> result;
+    stringstream ss(str);
+    string token;
+
+    while (getline(ss,token,' ')) {
+        result.push_back(token);
+    }
+    return result;
+}
+
+void Welcome(int TCP_socket) {
+    char sendMessage[512] = { "*****Welcome to Game 1A2B*****" };
+    int errS = send(TCP_socket, sendMessage, sizeof(sendMessage), 0);
+    if (errS == -1) {
+        cout << "[Error] Fail to receive message from the client." << endl;
+    }
+}
+
+void Register(int UDP_socket, vector<string>recVec, struct sockassr_in &clientAddr) {
+    string sendBack;
+    char sendMessage[512] = {};
+
+    map<string,string>::iterator itName = nameMap.find(recVec[1]);
+    map<string,string>::iterator itEmail = emailMap.find(recVec[2]);
+
+    if (recVec.size() != 4) {
+        sendBack = "Usage: register <username> <email> <password>";
+    }
+    else if (itName != nameMap.end()) {
+        sendBack = "Username is already used.";
+    }
+    else if (itEmail != emailMap.end()) {
+        sendBack = "Email is already used.";
+    }
+    else {
+        nameMap[recVec[1]] = recVec[3];
+        emailMap[recVec[2]] = recVec[1];
+        sendBack = "Register successfully.";
+    }
+
+    int len = sendBack.length();
+    sendBack.copy(sendMessage, len);
+    int errS = sendto(UDP_socket, sendMessage, sizeof(sendMessage), 0, (const strucr sockaddr*) &clientAddr, sizeof(clientAddr));
+    if (errS == -1) {
+        cout << "[Error] Fail to receive message from the client." << endl;
+    }
+}
+
+void Rule(int UDP_socket, struct sockaddr_in &clientAddr) {
+    string sendBack;
+    char sendMessage[512] = {};
+    
+    if (recVec.size() != 1) {
+        sendBack = "Usage: game-rule";
+    }
+    else {
+        sendBack = "1. Each question is a 4-digit secret number.\
+        \n2. After each guess, you will get a hint with the following information :\
+        \n2.1 The number of \"A\", which are digits in the guess that are in the correct position.\
+        \n2.2 The number of \"B\", which are digits in the guess that are in the answer but are in the wrong position.\
+        \nThe hint will be formatted as \"xAyB\".\
+        \n3. 5 chances for each question."
+    }
+
+    int len = sendBack.length();
+    sendBack.copy(sendMessage, len);
+
+    int errS = sendto(UDP_socket, sendMessage, sizeof(sendMessage), 0, (const struct sockaddr*) &clientAddr, sizeof(clientAddr));
+    if (errS == -1) {
+        cout << "[Error] Fail to send message from the client." << endl;
+    }
+}
+
+void Exit(int newClient, string user) {
+    string sendBack;
+    char sendMessage[512] = {};
+
+    if (recVecTCP.size() != 1) {
+        sendBack = "Usage: exit";
+    }
+    else if (user != "") {
+        sendBack = { "Please logout first." };
+    }
+    else {
+        close(newClient);
+        pthread_exit(0);
+    }
+
+    int len = sendBack.length();
+    sendBack.copy(sendMessage, len);
+
+    int errS = send(newClient, sendMessage, sizeof(sendMessage), 0);
+    if (errS == -1) {
+        cout << "[Error] Fail to send message from the client." << endl;
+    }
+}
+
+string Logout(int newClient, string user) {
+    char sendMessage[512] = {};
+    string sendBack;
+    string loginUser = user;
+
+    if (recVecTCP.size() != 1) {
+        sendBack = "Usage: logout";
+    }
+    else if (user == "") {
+        sendBack = "Please login first.";
+    }
+    else {
+        loginUser = "";
+        sendBack = "Bye, " + user + ".";
+    }
+
+    int len = sendBack.length();
+    sendBack.copy(sendMessage, len);
+    int errS = send(newClient, sendMessag ,sizeof(sendMessag), 0);
+    if (errS == -1) {
+        cout << "[Error] Fail to send message from the client." << endl;
+    }
+
+    return loginUser;
+}
+
+string Login(int newClient, vector<string> recVecTCP, string user) {
+    char sendMessage[512] = {};
+    string loginUser = user;
+    string sendBack;
+
+    map<string,string>::iterator it = nameMap.find(recVecTCP[1]);
+
+    if (recVecTCP.size() != 3) {
+        sendBack = "Usage: login <username> <password>";
+    }
+    else if (user != "") {
+        sendBack = "Please logout first.";
+    }
+    else if (it == nameMap.end()) {
+        sendBack = "Username not found.";
+    }
+    else if (nameMap[recVecTCP[1]] != recVecTCP[2]) {
+        sendBack = "Password not correct.";
+    }
+    else {
+        loginUser = recVecTCP[1];
+        sendBack = "Welcome, "+ recVecTCP[1] + ".";
+    }
+
+    int len = sendBack.length();
+    sendBack.copy(sendMessage, len);
+    int errS = send(newClient, sendMessage, sizeof(sendMessage), 0);
+    if (errS == -1) {
+        cout << "[Error] Fail to send message to the client." << endl;
+    }
+
+    return loginUser;
+}
+
+bool check4digits(string num) { 
+    if (num.length() != 4) return false;
+
+    const char* ch = num.c_str();
+    for (int i=0; i<4; i++) {
+        if (ch[i]<48 || ch[i]>57) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void Start(int newClient, vector<string> recVecTCP, string user) {
+    string sendBack;
+    string ans = "";
+    char sendMessage[512] = {};
+
+    if (recVecTCP.size() > 2) {
+        sendBack = "Usage: start-game <4-digit number>";
+    }
+    else if (user == "" ) {
+        sendBack = "Please login first.";
+    }
+    else if (recVecTCP.size() == 2) {
+        bool check = check4digits();
+        if (!check) {
+            sendBack = "Usage: start-game <4-digit number>";
+        }
+        else {
+            ans = recVecTCP[1];
+            sendBack = "Please typing a 4-digit number:";
+        }
+    }
+    else if (recVecTCP.size() == 1) {
+        srand(time(NULL));
+        int randomAnswer = rand() % 10000;
+        stringstream ss;
+        ss << randomAnswer;
+        string str = ss.str();
+        ans = str;
+        sendBack = "Please typing a 4-digit number:";
+    }
+    
+    int len = sendBack.length();
+    sendBack.copy(sendMessage, len);
+    int errS = send(newClient, sendMessage, sizeof(sendMessage), 0);
+    if (errS == -1) {
+        cout << "[Error] Fail to send message to the client." << endl;
+    }
+
+    if (ans != "") {
+        Game(newClient, ans);
+    }
+}
+
+void Game(int newClient, string ans) {
+    char receiveMessage[512] = {};
+    string input, sendBack;
+    int chance = 5
+
+    while (chance > 0) {
+        int errR = recv(newClient, receiveMessage, sizeof(receiveMessage), 0);
+        if (errR == -1) {
+            cout << "[Error] Fail to receive message from the client." << endl;
+        }
+
+        input = receiveMessage;
+        bool check = check4digits(input);
+        if (!check) {
+            sendBack = "Your guess should be a 4-digit number.";
+        }
+        
+    }
+    
+}
+
+void* Connection(void* data) {
+    int newClient = *((int*) data);
+    char receiveMessage[512] = {};
+    vector<string> recVecTCP;
+    string loginUser = "";
+    char ans[4];
+
+    while (1) {
+        int errR = recv(newClient, receiveMessage, sizeof(receiveMessage),0);
+        if (errR == -1) {
+            cout << "[Error] Fail to receive message from the client." << endl;
+            return 0;
+        }
+
+        recVecTCP = split(receiveMessage);
+
+        if (recVecTCP[0] == "exit") {
+            Exit(newClient, loginUser);
+        }
+        else if (recVecTCP[0] == "login") {
+            loginUser = Login(newClient, recVecTCP, loginUser);
+        }
+        else if (recVecTCP[0] == "logout") {
+            Logout(newClient, loginUser);
+        }
+        else if (recVecTCP[0] == "start-game") {
+            Start();
+        }
+        else {
+            cout << "[Error] receive command not found." << endl;
+        }
+    }
+}
+
 int main(int argc, char* argv[]) {  
 
     int serverPort;
     int TCP_socket,UDP_socket;  
-    struct sockaddr_in serverAddr;  
-    struct sockaddr_in clientAddr; 
-    int connect_socket;  
+    struct sockaddr_in serverAddr;   
 
     // check command format 
     const char* s = "./server";
@@ -34,13 +309,13 @@ int main(int argc, char* argv[]) {
     // create TCP and UDP socket  
     TCP_socket = socket(AF_INET, SOCK_STREAM, 0); 
     if (TCP_socket == -1) {
-        cout << "Error: Fail to create a TCP socket." << endl;
+        cout << "[Error] Fail to create a TCP socket." << endl;
         return 0;
     }  
 
     UDP_socket = socket(AF_INET, SOCK_DGRAM, 0); 
     if (UDP_socket == -1) {
-        cout << "Error: Fail to create a UDP socket." << endl;
+        cout << "[Error] Fail to create a UDP socket." << endl;
         return 0;
     } 
 
@@ -53,43 +328,74 @@ int main(int argc, char* argv[]) {
     // bind a socket to a local IP address and a port number
     int errBT = bind(TCP_socket, (struct sockaddr*) &serverAddr, sizeof(serverAddr));  
     if (errBT == -1) {
-        cout << "Error: Fail to bind a TCP socket." << endl;
+        cout << "[Error] Fail to bind a TCP socket." << endl;
         return 0;
     }
     int errBU = bind(UDP_socket, (struct sockaddr*) &serverAddr, sizeof(serverAddr));  
     if (errBU == -1) {
-        cout << "Error: Fail to bind a UDP socket." << endl;
+        cout << "[Error] Fail to bind a UDP socket." << endl;
         return 0;
     }
     
     // wait for someone to connect to my port(used by servers)
     int errL = listen(TCP_socket, MAX_CLIENT);  
     if (errL == -1) {
-        cout << "Error: Fail to listen." << endl;
+        cout << "[Error] Fail to listen." << endl;
         return 0;
     }
     
-    char buffer[256] = {};
-    int pid;
+    fd_set set;
+    FD_ZERO(&set);
+    int nfds = max(TCP_socket, UDP_socket) + 1;
+    int newClient;
+    struct sockaddr_in clientAddr;
+    char receiveMessage[512] = {};
+    vector<string> recVec;
+    pthread_t pid;
 
     while (1) {
-        // accept a new connection and get a new socket for subsequent data transmissions   
-        connect_socket = accept(TCP_socket, (struct sockaddr*) &clientAddr, &sizeof(clientAddr));  
-        cout << "New connection." << endl;
-        
-        pid = fork();
-        if (pid == 0) {
-            // TCP_socket -> connect_socket
-            close(TCP_socket);
-
-            while (1) {
-                FD_SET(connect_socket, &rset);
-                FD_SET(UDP_socket, &rset);
-            }
+        FD_SET(TCP_socket, &set);
+        FD_SET(UDP_socket, &set);
+        int errSEL = select(nfds, &set, NULL, NULL, NULL);
+        if (errSEL == -1) {
+            cout << "[Error] Fail to select." << endl;
+            return 0;
         }
-        // close the socket   
-        close(connect_socket);   
-        close(UDP_socket);  
-        return 0; 
+
+        // message is sent by TCP
+        if (FD_ISSET(TCP_socket, &set)) {
+            newClient = accept(TCP_socket, (struct addr*) &clientAddr, sizeof(clientAddr));
+            cout << "New Connection." << endl;
+            Welcome(newClient);
+            pthread_create(&pid, NULL, Connection, &newClient);
+        }
+        
+        // message is sent by UDP
+        if (FD_ISSET(UDP_socket, &set)) {
+            int errR = recvfrom(UDP_socket, receiveMessage, sizeof(receiveMessage),0, (struct sockaddr*) &clientAddr, sizeof(clientAddr));
+            if (errR == -1) {
+                cout << "[Error] Fail to receive message from the client." << endl;
+                return 0;
+            }
+            
+            recVec = split(receiveMessage);
+
+            if (recVec[0] == "register") {
+                Register(UDP_socket, recVec, clientAddr);
+            }
+            else if (recVec[0] == "game-rule") {
+                Rule(UDP_socket, clientAddr);
+            }
+            else {
+                cout << "[Error] receive command not found." << endl;
+            }
+            
+            memset(receiveMessage, '\0', sizeof(receiveMessage));
+            recVec.clear();
+        }
     }
+    
+    close(TCP_socket);
+    close(UDP_socket);
+    return 0;
 }  
